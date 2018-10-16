2f1640c7dd9ad1151296364dd1e73e5680c062c0
SoojungChae <naan_ace@naver.com>
2018-10-16 09:50:24 +0900

    Add css loader

--- app/App.js
+++ app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './Login';
 import Grid from './Grid';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './assets/css/style.css';
 
 const App = () => (
     <Router>
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 module.exports = {
-	entry:  __dirname + "/app/Main.js",
-	output: {
-		path: __dirname + "/public",
-		filename: "bundle.js"
-	},
-	module: {
-		loaders: [{
-			test: /\.jsx?$/,
-			loader: 'babel',
-			query:{
-				cacheDirectory: true,
-				presets: ['react','es2015']
-			}
-		}]
-	}
+    entry:  __dirname + "/app/Main.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                query:{
+                    cacheDirectory: true,
+                    presets: ['react','es2015']
+                }
+            }, {
+                test: /\.css$/,
+                loader: 'style!css',
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }
 };
